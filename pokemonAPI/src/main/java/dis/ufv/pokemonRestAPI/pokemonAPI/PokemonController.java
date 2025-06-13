2c50c37773ddb16160ca60cd060f3b3594b04b85
Luciablaz <luciablazquezgallardo@gmail.com>
2025-06-13 18:57:38 +0200

    Metodo añadido en PokemonController para filtrar por pokemon

--- pokemonAPI/src/main/java/dis/ufv/pokemonRestAPI/pokemonAPI/PokemonController.java
+++ pokemonAPI/src/main/java/dis/ufv/pokemonRestAPI/pokemonAPI/PokemonController.java
@@ -1,6 +1,9 @@
 package dis.ufv.pokemonRestAPI.pokemonAPI;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -11,6 +14,18 @@ public class PokemonController {
     public ArrayList<Pokemon> pokemons(){
         ArrayList<Pokemon> listaPokemons = new LectorJson().leeFicheroJson();
         return listaPokemons;
+    }
+
+    @GetMapping("/pokemon/porNombre/{nombre}")
+    public ResponseEntity<Pokemon> getPorNombre(@PathVariable String nombre){
+        ArrayList<Pokemon> listaPokemons = new LectorJson().leeFicheroJson();
+        Pokemon encontrado = null;
+        for (Pokemon pokemon : listaPokemons) {
+            if (pokemon.getName().equalsIgnoreCase(nombre)) {
+                encontrado = pokemon;
+            }
+        }
+        return new ResponseEntity<>(encontrado, HttpStatus.OK);
     }
 }
 
