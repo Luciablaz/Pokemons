1adfc97ba5173c9fba1f30356a70f06ed2ae16d2
Luciablaz <luciablazquezgallardo@gmail.com>
2025-06-13 18:30:39 +0200

    Clase LectorJson creada

---
+++ pokemonAPI/src/main/java/dis/ufv/pokemonRestAPI/pokemonAPI/LectorJson.java
@@ -0,0 +1,23 @@
+package dis.ufv.pokemonRestAPI.pokemonAPI;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class LectorJson {
+    public ArrayList<Pokemon> leerFicheroJson(String fichero){
+        try {
+            //lee el fichero que le pasemos y lo carga en un reader
+            Reader reader = Files.newBufferedReader(Paths.get(fichero));
+            // convierte el array JSON a un arraylist de users
+            ArrayList<Pokemon> listaPokemon =
+                    new Gson().fromJson(reader, new TypeToken<ArrayList<Pokemon>>() {}.getType());
+            reader.close();// close reader
+            return listaPokemon;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ArrayList<>(); //si no ha leido nada, devuelve un array vacio
+        }
+    }
+}
