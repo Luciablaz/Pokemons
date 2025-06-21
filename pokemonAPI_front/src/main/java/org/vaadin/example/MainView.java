80e877662ec4e99f9e46acf5229fe8b7f5ba49b3
Luciablaz <luciablazquezgallardo@gmail.com>
2025-06-21 12:21:04 +0200

    Clase MainView actualizada

--- pokemonAPI_front/src/main/java/org/vaadin/example/MainView.java
+++ pokemonAPI_front/src/main/java/org/vaadin/example/MainView.java
@@ -1,5 +1,7 @@
 package org.vaadin.example;
 
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.grid.Grid;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.flow.component.Key;
@@ -7,6 +9,7 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
@@ -34,29 +37,35 @@ public class MainView extends VerticalLayout {
      *            The message service. Automatically injected Spring managed
      *            bean.
      */
-    public MainView(@Autowired GreetService service) {
+    public MainView(@Autowired PokemonService service) {
+        HorizontalLayout inputs = new HorizontalLayout();
+        VerticalLayout results = new VerticalLayout();
+        ComboBox<String> comboBox = new ComboBox<>("Selecciona uno...");
+        comboBox.setAllowCustomValue(false);
+        comboBox.setItems("Todos los pokemons", "Por Nombre", "Por tipo");
+        comboBox.setHelperText("Selecciona el tipo de petición");
 
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
-        textField.addThemeName("bordered");
-
-        // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Say hello",
-                e -> Notification.show(service.greet(textField.getValue())));
-
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button has a more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
-
-        // Use custom CSS classes to apply styling. This is defined in
-        // shared-styles.css.
+        Grid<Pokemon> grid = new Grid<>(Pokemon.class, true);
+        grid.addColumn(Pokemon::getName).setHeader("Nombre");
+        grid.addColumn(Pokemon::getAttack).setHeader("Ataque");
+        grid.addColumn(Pokemon::getDefense).setHeader("Defensa");
+        grid.addColumn(Pokemon::getTipo1).setHeader("Tipo1");
+        grid.addColumn(Pokemon::getSpeedDefense).setHeader("Tipo2");
+        inputs.add(comboBox);
+        Button boton1 = new Button("Lee caracter",
+                e -> {
+                    String tipoPeticion = comboBox.getValue();
+                    try {
+                        results.removeAll();
+                        results.add(service.leePokemon(tipoPeticion));
+                    } catch (Exception ex) {
+                    }
+                });
+        boton1.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        boton1.addClickShortcut(Key.ENTER);
         addClassName("centered-content");
-
-        add(textField, button);
+        add(inputs, boton1, results);
     }
+
 
 }
